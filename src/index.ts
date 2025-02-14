9ad426c4e31c0730e2f43bb4ecd3a39af41f376e
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-14 15:20:26 -0600

    triggering callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Myname", age: 23 });
 
-user.on("change", () => {});
-user.on("change", () => {});
-user.on("sf", () => {});
+user.on("change", () => {
+  console.log("change #1");
+});
+user.on("change", () => {
+  console.log("change 2");
+});
+user.on("save", () => {
+  console.log("save was triggered");
+});
+
+user.trigger("save");
 
 console.log(user);
