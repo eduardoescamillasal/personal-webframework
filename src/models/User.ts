d41f1ef75adc7e794508ec81f3d3e5eb818713f2
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-16 11:20:22 -0600

    wiring attributes into user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,8 @@
+import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
@@ -12,14 +13,9 @@ const rootUrl = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
