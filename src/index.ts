2c208f084dd7a84c3d73ae42ec66ef56adc4fca0
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-18 09:40:27 -0600

    save method on user

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = new User({ id: 1, name: "neweer name", age: 989 });
 
 console.log(user.get("name"));
 
-user.on("change", () => {
+user.on("save", () => {
   console.log(user);
 });
-user.fetch();
+user.save();
